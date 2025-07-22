009789d8c244f6ee2bd691a61f5a2062cb9916e6
Vishwa Janek 333 <vishwajrevacollegeaiandds@gmail.com>
2025-07-22 05:32:32 +0000

    check while solving

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);// this convert not bcs its not good pratice 
+// console.log("02">1);
+
+console.log(null>0);// false comparsion little diiferent  chk in google it in js
+console.log(null==0);// false
+console.log(null>=0);//true
+
+console.log(undefined == 0);//false
+console.log(undefined >0);//false
+console.log(undefined < 0);//false
+
+/// === its strict  check
+
+console.log("2"===2);
+
