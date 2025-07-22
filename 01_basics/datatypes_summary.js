694135a8506d820a782e222dc8f98f1b117cb50b
Vishwa Janek 333 <vishwajrevacollegeaiandds@gmail.com>
2025-07-22 06:31:59 +0000

    file

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+/// primtive
+// 7types :String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100 // js is scrpited, dynamic type of language and it run client side +server side.
+const scoreValue =100.3
+
+const isLoggedIn =false
+const anotherId= Symbol('123')
+const id=123
+console.log(id === anotherId);// just for check 
+
+const bigNumber =343333534348239n
+
+
+
+/// reference (non primitive)
+
+// Array,objects,functions
+const colours=["red","blue","green"];
+let myObj={
+    name:"vish",
+    age:21,
+
+}
+const myFunction = function(){
+    console.log("hello");
+}
+ console.log(typeof variables_name) // it  tells about the type of variable
+
+
+ //+++++++++++++++++++++++++++++++++++++++++
+ //
