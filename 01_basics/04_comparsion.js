e27bd960748b191f6edd93ab767af8b4cf9d5add
Vishwa Janek 333 <vishwajrevacollegeaiandds@gmail.com>
2025-07-27 05:53:54 +0000

    scope1

--- 01_basics/04_comparsion.js
+++ 01_basics/04_comparsion.js
@@ -1,10 +1,10 @@
 // console.log(2>1)
 // console.log(2>=1)
 // console.log(2<1)
-// console.log(2==1)
+// console.log(2==2)
 // console.log(2!=1)
 
-// console.log("2">1);// this convert not bcs its not good pratice 
+//  console.log("2">1);// this convert not bcs its not good pratice 
 // console.log("02">1);
 
 console.log(null>0);// false comparsion little diiferent  chk in google it in js
---
+++ 03_basics/02_scope.js
@@ -0,0 +1,23 @@
+
+// scope  main see in for loops , if else condition  etc so many
+// scope  use based  on global and block or local
+
+if(true){
+    let a=3
+    var b=8
+    const c=5 
+    console.log(c)
+}
+//console.log(a)
+console.log(b)
+//console.log(c)
+
+for(let i=0;i<Array.length;i++){
+    const element =array[i]
+}
+
+//scope  is running in node in local and but scope is differnt in console in goole inspect
+
+
+
+
