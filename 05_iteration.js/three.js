6068be6c86ed537fd940d51470674716db741da6
Vishwa Janek 333 <vishwajrevacollegeaiandds@gmail.com>
2025-07-29 13:40:43 +0000

    for of

---
+++ 05_iteration.js/three.js
@@ -0,0 +1,35 @@
+// for of
+
+// ["","",""]
+//[{},{},{}]
+
+const arr=[1,2,3,4,5]
+
+for(const nums of arr){
+    console.log(nums);
+
+}
+const greetings = "hello World!"
+for(const greet of greetings){
+    console.log(`each char is ${greet}`)
+}
+
+//maps : unique values
+
+const map =new Map()
+map.set ('IN',"INDIA")
+map.set ('usa',"usa")
+map.set ('fr',"france")
+map.set('IN',"INDIA")
+
+console.log(map);
+
+for(const key of map){
+    console.log(key,':-',value);
+}
+// objects  general not iterate
+const myObject ={
+    'game1':'NFS'
+}
+
+
